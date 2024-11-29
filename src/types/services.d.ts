0f56ae853613fcf8adb6e6f610a19eb42aca81e1
dennisrijsdijk <git@dennis.gg>
2024-11-29 22:41:58 +0100

    chore(types): ActivityFeedService

--- src/types/services.d.ts
+++ src/types/services.d.ts
@@ -14,3 +14,34 @@ export interface AccountAccessService {
     getAccounts: () => void;
     logoutAccount: (accountType: "streamer" | "bot") => void;
 }
+
+export interface Activity {
+    id: unknown;
+    source: {
+        id: string;
+        name: string;
+    };
+    event: {
+        id: string;
+        name: string;
+        forceAllow: boolean;
+        canRetrigger: boolean;
+    };
+    message: string;
+    icon: string;
+    acknowledged: boolean;
+    timestamp: string;
+}
+
+export interface ActivityFeedService {
+    allActivities: Activity[];
+    /** Activities shown in the activity feed */
+    activities: Activity[];
+    allAcknowledged: () => boolean;
+    markAllAcknowledged: () => void;
+    markAllNotAcknowledged: () => void;
+    toggleMarkAllAcknowledged: () => void;
+    unacknowledgedCount: () => number;
+    retriggerEvent: (activityId: string) => void;
+    showEditActivityFeedEventsModal: () => void;
+}
